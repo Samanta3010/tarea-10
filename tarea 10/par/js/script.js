1e90ab104850ca0881885fa5bce1839f3c0e8153
Samanta3010 <124941489+Samanta3010@users.noreply.github.com>
2024-09-08 23:30:08 -0300

    Add files via upload

---
+++ tarea 10/par/js/script.js
@@ -0,0 +1,13 @@
+document.getElementById('numerosForm').addEventListener('submit', function(event) {
+     
+    const num1 = Number(document.getElementById('num1').value);
+    const num2 = Number(document.getElementById('num2').value);
+    const num3 = Number(document.getElementById('num3').value);
+    const num4 = Number(document.getElementById('num4').value);
+
+   
+    const mayor = Math.max(num1, num2, num3, num4);
+
+   
+    alert(El número mayor es: ${mayor});
+});
